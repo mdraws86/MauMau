a14e162f509d73e91e4caceaeda0ae880726c8bd
mdraws86 <m.draws@hotmail.de>
2024-05-01 13:01:08 +0200

    Make changes for the package

--- package/setup.py
+++ package/setup.py
@@ -1,1 +1,8 @@
 from setuptools import setup
+
+setup(name = "play_uno",
+      version = "0.1",
+      description = "Play UNO in the command line",
+      packages = ["play_uno"],
+      zip_safe = False
+      )
